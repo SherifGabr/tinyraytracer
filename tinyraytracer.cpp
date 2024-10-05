7adffd0c55ea70f07118fe5ae8c56f38918a4e25
Sherif Gabr <sgabr21@student.oulu.if>
2024-10-05 22:53:22 +0300

    Add refraction

--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -15,18 +15,21 @@ struct Light {
 };
 
 struct Material {
-	Material(const Vec3f& a, const Vec3f& color, const float spec) :
+	Material(const float &r, const Vec4f& a, const Vec3f& color, const float spec) :
+		refractive_index(r),
 		albedo(a),
 		diffuse_color(color),
 		specular_exponent(spec) {}
 	Material() :
-		albedo(1, 0, 0),
+		refractive_index(1),
+		albedo(1, 0, 0, 0),
 		diffuse_color(),
 		specular_exponent() {}
 
-	Vec3f albedo;
+	Vec4f albedo;
 	Vec3f diffuse_color;
 	float specular_exponent;
+	float refractive_index;
 };
 
 struct Sphere {
@@ -68,6 +71,24 @@ Vec3f reflect(const Vec3f &I, const Vec3f &N)
 	return I - N * 2.f * (I * N);
 }
 
+Vec3f refract(const Vec3f& I, const Vec3f& N, const float& refractive_index) {
+	float cosi = -std::max(-1.f, std::min(1.f, I * N));
+	float etai = 1;
+	float etat = refractive_index;
+	Vec3f n = N;
+
+	if (cosi < 0) { // total internal reflection
+		cosi = -cosi;
+		std::swap(etai, etat);
+		n = -N;
+	}
+
+	float eta = etai / etat;
+	float k = 1 - eta * eta*(1 - cosi * cosi);
+
+	return k < 0 ? Vec3f(0, 0, 0) : I * eta + n * (eta * cosi - sqrtf(k));
+}
+
 bool scene_intersect(const Vec3f& orig, const Vec3f& dir, const std::vector<Sphere>& spheres, Vec3f& hit, Vec3f& N, Material& material) {
 	float spheres_dist = std::numeric_limits<float>::max();
 	for (size_t i = 0; i < spheres.size(); i++) {
@@ -91,8 +112,12 @@ Vec3f cast_ray(const Vec3f& orig, const Vec3f& dir, const std::vector<Sphere>& s
 		return Vec3f(0.2, 0.7, 0.8); // background color
 
 	Vec3f reflect_dir = reflect(dir, N).normalize();
-	Vec3f reflect_orig = reflect_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3;
-	Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1); // recurse
+	Vec3f reflect_orig = reflect_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3; // offsets the original point to avoid occlusion by the object itself
+	Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth + 1); // recurses
+
+	Vec3f refract_dir = refract(dir, N, material.refractive_index).normalize();
+	Vec3f refract_orig = refract_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3; // offsets the original point to avoid occlusion by the object itself
+	Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth + 1); // recurses
 
 	float diffuse_light_intensity = 0;
 	float specular_light_intensity = 0;
@@ -111,7 +136,8 @@ Vec3f cast_ray(const Vec3f& orig, const Vec3f& dir, const std::vector<Sphere>& s
 	}
 	return material.diffuse_color * diffuse_light_intensity * material.albedo[0] +
 		   Vec3f(1., 1., 1.) * specular_light_intensity * material.albedo[1] +
-		   reflect_color * material.albedo[2];
+		   reflect_color * material.albedo[2] +
+		   refract_color * material.albedo[3];
 }
 
 void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights)
@@ -154,17 +180,19 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
 
 int main()
 {
-	Material ivory(Vec3f(0.6, 0.3, 0.1), Vec3f(0.4, 0.4, 0.3), 50.);
-	Material red_rubber(Vec3f(0.9, 0.1, 0.0), Vec3f(0.3, 0.1, 0.1), 10.);
-	Material mirror(Vec3f(0.0, 10.0, 0.8), Vec3f(1.0, 1.0, 1.0), 1450.);
+	Material ivory(1.0, Vec4f(0.6, 0.3, 0.1, 0.0), Vec3f(0.4, 0.4, 0.3), 50.);
+	Material glass(1.5, Vec4f(0.0, 0.5, 0.1, 0.8), Vec3f(0.6, 0.7, 0.8), 125.);
+	Material red_rubber(1.0, Vec4f(0.9, 0.1, 0.0, 0.0), Vec3f(0.3, 0.1, 0.1), 10.);
+	Material mirror(1.0, Vec4f(0.0, 10.0, 0.8, 0.0), Vec3f(1.0, 1.0, 1.0), 1425.);
 
 	std::vector<Sphere> spheres;
 	spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
-	spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
+	spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, glass));
 	spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber));
 	spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, mirror));
 
 	std::vector<Light>  lights;
+	lights.push_back(Light(Vec3f(-20, 20, 20), 1.5));
 	lights.push_back(Light(Vec3f(30, 50, -25), 1.8));
 	lights.push_back(Light(Vec3f(30, 20, 30), 1.7));
 
